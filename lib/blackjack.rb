093dfbef3c5acd0059942273396be27df9ece323
Coral Fussman <coralfussman@gmail.com>
2020-03-29 21:23:14 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -27,10 +27,9 @@ puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
-card_total = deal_card + deal_card
-display_card_total(card_total)
-deal_card(card_total)
-display_card_total(card_total)
+
+display_card_total(card_total,card_total)
+
 end
 
 def hit?
