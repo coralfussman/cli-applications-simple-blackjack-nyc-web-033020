663af156ac8341ccfe867807214d5fff1e3673d8
Coral Fussman <coralfussman@gmail.com>
2020-03-29 21:21:48 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -4,26 +4,33 @@ end
 
 def deal_card
 
+  rand(1..11)
+
 end
 
-def display_card_total
+def display_card_total(card_total)
+
+puts "Your cards add up to #{card_total}"
   # code #display_card_total here
 end
 
 def prompt_user
-  # code #prompt_user here
+puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+gets.chomp
 end
 
-def end_game
-  # code #end_game here
+def end_game(card_total)
+puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+card_total = deal_card + deal_card
+display_card_total(card_total)
+deal_card(card_total)
+display_card_total(card_total)
 end
 
 def hit?
